564b80ead7be1996b5fa9923d80fadc84e81706a
Toti Muñoz <totigm@gmail.com>
2022-06-29 04:13:16 -0300

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export { default } from "./bot";
+import Bot from "./bot";
+
+module.exports = Bot;
+export default Bot;
+
 export { BaseMessage, Message, Command, CommandHandler, HandlerMessage, Documentation, PartialOptions as Options } from "./types";
